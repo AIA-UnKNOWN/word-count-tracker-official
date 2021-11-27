7b41666fe129195445beb9c4f89086da188f5e75
Ajboy Ian Abordo <aabordo@kodeacross.com>
2021-11-27 11:47:00 +0800

    Implemented the Weekly Tracker logic for every week

--- resources/js/components/WeeklyTracker.js
+++ resources/js/components/WeeklyTracker.js
@@ -1,13 +1,81 @@
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 
 import WeekTracker from './weekly-tracker/Week';
 
 
 const WeeklyTracker = ({ date }) => {
+  const [weeks, setWeeks] = useState({
+    firstWeek: [],
+    secondWeek: [],
+    thirdWeek: [],
+    fourthWeek: [],
+    fifthWeek: []
+  });
   
   useEffect(() => {
-    
+    const lastMonthDays = getPreviousMonthDays();
+    const currentMonthDays = getCurrentMonthDays();
+    const nextMonthDays = getNextMonthDays();
+
+    const calendarDays = [...lastMonthDays, ...currentMonthDays, ...nextMonthDays];
+
+    runWeeklyTracker(calendarDays);
   }, [date]);
+
+  const getPreviousMonthDays = () => {
+    const lastDayOfLastMonth = date.getLastDayOfLastMonth();
+    const firstDayOfTheMonthIndex = date.getFirstDayIndex();
+    const remainingDaysForFirstWeek = 7 - (firstDayOfTheMonthIndex + 1);
+    const lastDaysOfLastMonth = 7 - (remainingDaysForFirstWeek + 1);
+    const startingCalendarDay = (lastDayOfLastMonth - lastDaysOfLastMonth) + 1;
+    const days = [];
+
+    for (let day = startingCalendarDay; day <= lastDayOfLastMonth; day++) {
+      days.push(day);
+    }
+
+    return days;
+  }
+
+  const getCurrentMonthDays = () => {
+    const days = [];
+
+    for (let day = 1; day <= date.getLastDayOfCurrentMonth(); day++) {
+      days.push(day);
+    }
+
+    return days;
+  }
+
+  const getNextMonthDays = () => {
+    const lastDayOfTheMonthIndex = date.getLastDayOfCurrentMonthIndex();
+    const remainingDaysForFinalWeek = 7 - (lastDayOfTheMonthIndex + 1);
+    const days = [];
+
+    for (let day = 1; day <= remainingDaysForFinalWeek; day++) {
+      days.push(day);
+    }
+
+    return days;
+  }
+
+  const runWeeklyTracker = calendarDays => {
+    const days = [...calendarDays];
+    const calendarWeeks = {...weeks};
+    const calendarWeeksList = Object.keys(calendarWeeks); // Returns an array of {Object} keys
+    const calendarWeeksPerMonth = calendarWeeksList.length;
+
+    for (let i = 0; i < calendarWeeksPerMonth; i++) {
+      const week = calendarWeeksList[i];
+
+      for (let i = 0; i < 7; i++) {
+        const day = days.shift();
+        calendarWeeks[week].push(day);
+      }
+    }
+
+    setWeeks(calendarWeeks);
+  }
 
   return (
     <div className="weekly-tracker-container">
